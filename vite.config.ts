7e9a9b02780c2d7f6a7c4e1fd3e2ffe9e1c9e885
Georgi Iliev <georgi.iliev533@outlook.com>
2023-02-08 20:08:34 +0200

    New deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://georgi-m-iliev.github.io/SpotifyCurrentlyPlaying",
+  base: "/SpotifyCurrentlyPlaying",
   plugins: [react()],
 })
